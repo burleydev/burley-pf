9ceb4b53d9fac0f077d8a656a6c7a7194ee78375
Carlos Borrega <c.borrega@proton.me>
2024-09-16 16:11:39 +0100

    updated about me section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,27 +3,33 @@ import '../index.css';
 import Profile from '../assets/profile.png';
 
 const About = () => (
-    <div id='about' className='grid grid-cols-5 place-items-center mx-[4rem] h-screen'>
-        <div className='col-span-2'>
-            <img src={Profile} alt='Carlos Borrega' className='rounded-l-xl' />
-        </div>
-        <div className='bg-white col-span-3 p-8 h-[34rem] rounded-r-xl'>
-            <p className='text-black my-6 text-sm leading-6'>
-                Hello, I'm Carlos. I am a front-end developer specializing in JavaScript, driven by a passion for constant learning. My background in retail management has honed my communication and teamwork skills, emphasizing my creative problem-solving abilities and adaptability to changes.
-            </p>
-            <p className='text-black my-6 text-sm leading-6'>
-                I graduated from an extensive coding bootcamp by edX and earned a certificate in front-end web development. Throughout the four-month program, I successfully balanced my coursework with work commitments, showcasing both quick learning abilities and a strong commitment to continuous improvement.
-            </p>
-            <p className='text-black my-6 text-sm leading-6'>
-                Detail-oriented and highly motivated, I derive satisfaction from coding efficient and responsive websites. I am eager to contribute my diverse skill set to dynamic development projects, demonstrating an unwavering commitment to excellence.
-            </p>
-            <button className='button'>
-                <a className='resume' href='./assets/docs/carlos-borrega_frontend-developer_resume.pdf' download='carlos-borrega_frontend-developer_resume'>
-                    Download my resume
-                </a>
-            </button>
-        </div>
+<div id='about' className='h-screen'>
+  <div className='grid grid-cols-5 h-full px-[10rem] py-[5rem]'>
+    <div className='profile col-span-2 flex'>
+      <img src={Profile} alt='Carlos Borrega' className='rounded-l-xl object-cover' />
     </div>
+    <div className='description bg-white p-8 rounded-r-xl col-span-3 flex items-center'>
+      <div>
+        <p className='text-black my-6 text-sm leading-6'>
+          Hello, I'm Carlos. I am a front-end developer specializing in JavaScript, driven by a passion for constant learning. My background in retail management has honed my communication and teamwork skills, emphasizing my creative problem-solving abilities and adaptability to changes.
+        </p>
+        <p className='text-black my-6 text-sm leading-6'>
+          I graduated from an extensive coding bootcamp by edX and earned a certificate in front-end web development. Throughout the four-month program, I successfully balanced my coursework with work commitments, showcasing both quick learning abilities and a strong commitment to continuous improvement.
+        </p>
+        <p className='text-black my-6 text-sm leading-6'>
+          Detail-oriented and highly motivated, I derive satisfaction from coding efficient and responsive websites. I am eager to contribute my diverse skill set to dynamic development projects, demonstrating an unwavering commitment to excellence.
+        </p>
+        <button>
+          <a href='./assets/docs/carlos-borrega_frontend-developer_resume.pdf' download='carlos-borrega_frontend-developer_resume'>
+            Download my CV <i class="fa-solid fa-circle-check"></i>
+          </a>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 );
 
 export default About;
