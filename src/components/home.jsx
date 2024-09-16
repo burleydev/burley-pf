b4b40da867d126d599e744208c81a9a300d6e3a9
Carlos Borrega <c.borrega@proton.me>
2024-09-16 10:55:43 +0100

    updated about section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,12 +3,10 @@ import '../index.css'
 
 const Home = () => {
   return (
-    <section className='home'>
         <div className='flex flex-col items-center justify-center h-screen'>
             <h1 className='my-4 text-7xl font-bold' data-aos='fade-up'>Hello, I'm Carlos</h1>
             <h2 className='my-4 text-2xl' data-aos='fade-up' data-aos-delay='600'>I'm a UX/UI and Frontend Engineer</h2>
         </div>
-    </section>
   );
 };
 
