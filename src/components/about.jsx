6fd05bb7df6d1a82be35f63ef802390781ece2e8
Carlos Borrega <c.borrega@proton.me>
2024-09-17 12:43:06 +0100

    fixed about in xs view

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,11 +4,11 @@ import Profile from '../assets/profile.png';
 
 const About = () => (
   <div id='about' className='h-screen xs:h-auto'>
-    <div className='grid h-full md:px-40 sm:py-20 lg:grid-cols-5 xs:grid-cols-1 xs:px-16'>
+    <div className='grid h-full md:px-40 sm:py-20 lg:grid-cols-5 xs:grid-cols-1 xs:px-0'>
       <div className='profile col-span-2 flex'>
         <img src={Profile} alt='Carlos Borrega' className='object-cover' />
       </div>
-      <div className='description bg-white p-8 col-span-3 flex items-center'>
+      <div className='description bg-white p-8 col-span-3 flex items-center xs:p-2'>
         <div className='w-full'>
           {[
             "Hello, I'm Carlos. I am a front-end developer specializing in JavaScript, driven by a passion for constant learning. My background in retail management has honed my communication and teamwork skills, emphasizing my creative problem-solving abilities and adaptability to changes.",
@@ -18,7 +18,7 @@ const About = () => (
             <p key={idx} className='text-black hyphens-auto my-6 mx-5 lg:text-xl md:text-3xl xs:text-lg'>{text}</p>
           ))}
           <div className='flex justify-center mt-6 lg:justify-end lg:mr-6'>
-            <button className='lg:mr-2 md:p-5 md:my-6 xs:p-4'>
+            <button className='lg:mr-2 md:p-5 md:my-6 xs:p-4 mb-8'>
               <a href='./assets/docs/carlos-borrega_frontend-developer_resume.pdf' download='carlos-borrega_frontend-developer_resume' className='lg:text-[1.5rem] md:text-3xl lg:p-0 md:p-8 xs:text-xl'>
                 Download my CV
               </a>
