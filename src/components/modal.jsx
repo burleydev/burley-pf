1fcdf3a97c9230e45b603bf85d261456b095dfd3
Carlos Borrega <c.borrega@proton.me>
2024-11-06 10:31:02 +0000

    adjusted modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -16,7 +16,7 @@ function ImageModal({ imageSrc }) {
     <div className='grid'>
       <a href={imageSrc}
          onClick={openModal}
-         className='xs:font-bold xs:text-xs sm:text-sm italic xs:text-myGrey xs:mt-4 xs:mb-8 text-right'>
+         className='xs:font-bold xs:text-xs sm:text-sm italic xs:text-myGrey xs:mt-4 xs:mb-8 xs:mr-4 text-right'>
         Show mobile version
       </a>
 
@@ -25,11 +25,13 @@ function ImageModal({ imageSrc }) {
           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
           onClick={closeModal}
         >
-          <div 
-            className="bg-white p-4 rounded-lg max-w-xs w-full"
-            onClick={(e) => e.stopPropagation()} // Prevents closing when clicking on the image
-          >
-            <img src={imageSrc} alt="Mobile Version" className="w-full h-auto rounded-md" />
+          <div className="bg-white p-4 rounded-lg max-w-xs w-full">
+            <img 
+              src={imageSrc} 
+              alt="Mobile Version" 
+              className="w-full h-auto rounded-md" 
+              onClick={closeModal} // Close modal when clicking the image
+            />
           </div>
         </div>
       )}
@@ -38,3 +40,4 @@ function ImageModal({ imageSrc }) {
 }
 
 export default ImageModal;
+
