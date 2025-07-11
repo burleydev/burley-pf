8e2b6787ca13d1663c1025fe29cbe4ad1a50d3e6
Carlos Borrega <c.borrega@proton.me>
2025-07-11 12:36:56 +0100

    updatesl

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,7 +3,7 @@ import '../index.css';
 
 const Contact = () => {
   const links = [
-    { href: 'mailto:c.borrega@pm.me', label: 'Email' },
+    { href: 'mailto:cborrega0@gmail.com', label: 'Email' },
     { href: 'https://www.linkedin.com/in/carlosborrega/', label: 'LinkedIn' },
     { href: 'https://github.com/borregadev', label: 'GitHub' },
   ];
